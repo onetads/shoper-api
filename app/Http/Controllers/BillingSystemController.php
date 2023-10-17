9641da4900d32345cda5bc712e386a5dcf7221b8
Patryk Papiór <patryk.papior@fingoweb.com>
2023-10-17 10:52:51 +0200

    Prepare class for billing system

---
+++ app/Http/Controllers/BillingSystemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DreamCommerce\Exception\HandlerException;
+use DreamCommerce\Handler;
+use Illuminate\Http\Request;
+
+class BillingSystemController extends Controller
+{
+    protected Handler $handler;
+
+
+    /**
+     * @param Request $request
+     * @return void
+     * @throws \Exception
+     */
+    public function index(Request $request)
+    {
+        $entrypoint = $request->get('shop_url');
+
+        try {
+            $this->handler = new Handler(
+                $entrypoint,
+                config('app-store.app_id'),
+                config('app-store.app_secret'),
+                config('app-store.appstore_secret')
+            );
+
+            // subscribe to particular events
+            $this->handler->subscribe('install', [$this, 'installHandler']);
+            $this->handler->subscribe('upgrade', [$this, 'upgradeHandler']);
+            $this->handler->subscribe('billing_install', [$this, 'billingInstallHandler']);
+            $this->handler->subscribe('billing_subscription', [$this, 'billingSubscriptionHandler']);
+            $this->handler->subscribe('uninstall', [$this, 'uninstallHandler']);
+
+            $this->handler->dispatch();
+        } catch (HandlerException $ex) {
+            throw new \Exception('Handler initialization failed', 0, $ex);
+        }
+    }
+
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function dispatch(): void
+    {
+        try {
+            $this->handler->dispatch();
+        } catch (HandlerException $ex) {
+            if ($ex->getCode() == HandlerException::HASH_FAILED) {
+                throw new \Exception('Payload hash verification failed', 0, $ex);
+            }
+        }
+    }
+
+    public function installHandler(array $arguments)
+    {
+        // TODO:: implement
+    }
+
+    public function billingInstallHandler(array $arguments)
+    {
+        // TODO:: implement
+    }
+
+
+    public function upgradeHandler(array $arguments)
+    {
+        // TODO:: implement
+    }
+
+    public function uninstallHandler(array $arguments)
+    {
+        // TODO:: implement
+    }
+
+    public function billingSubscriptionHandler(array $arguments)
+    {
+        // TODO:: implement
+    }
+}
