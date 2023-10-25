1c7a709bb307e665904c38aae2693122dddc697c
Jakub <jakub.gloger@fingoweb.com>
2023-10-25 09:43:30 +0200

    add javascript from shoper

--- resources/views/configure.blade.php
+++ resources/views/configure.blade.php
@@ -1,3 +1,52 @@
+<html lang="pl">
+<head>
+    <meta name="referrer" content="origin">
+    <script src="https://dcsaascdn.net/js/dc-sdk-1.0.5.min.js"></script>
+    <script>
+        (function () {
+            'use strict';
+
+            var styles;
+
+            if (localStorage.getItem('styles')) {
+                styles = JSON.parse(localStorage.getItem('styles'));
+                injectStyles(styles);
+            }
+
+            window.shopAppInstance = new ShopApp(function (app) {
+                app.init(null, function (params, app) {
+                    if (localStorage.getItem('styles') === null) {
+                        injectStyles(params.styles);
+                    }
+                    localStorage.setItem('styles', JSON.stringify(params.styles));
+
+                    app.show(null, function () {
+                        app.adjustIframeSize();
+                    });
+                }, function (errmsg, app) {
+                    alert(errmsg);
+                });
+            }, true);
+
+            function injectStyles(styles) {
+                var i;
+                var el;
+                var sLength;
+
+                sLength = styles.length;
+                for (i = 0; i < sLength; ++i) {
+                    el = document.createElement('link');
+                    el.rel = 'stylesheet';
+                    el.type = 'text/css';
+                    el.href = styles[i];
+                    document.getElementsByTagName('head')[0].appendChild(el);
+                }
+            }
+        }());
+    </script>
+</head>
+
+<body>
 <div>
     <div>
         <form action="{{route('configure.save')}}" method="post">
@@ -10,7 +59,8 @@
             <br>
             <label for="substitute_product">
                 Podmieniać produkt?
-                <input type="checkbox" id="substitute_product" name="substitute_product" @if($substitute_product) checked @endif>
+                <input type="checkbox" id="substitute_product" name="substitute_product"
+                       @if($substitute_product) checked @endif>
             </label>
             <input type="submit">
             @if($errors->any())
@@ -30,3 +80,5 @@
         </form>
     </div>
 </div>
+</body>
+</html>
