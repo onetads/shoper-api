f0bd7ee7e8b9c9368da3ae29a727f9459030c82f
Jakub <jakub.gloger@fingoweb.com>
2023-10-25 10:04:45 +0200

    remove csrf token

--- resources/views/configure.blade.php
+++ resources/views/configure.blade.php
@@ -2,55 +2,13 @@
 <head>
     <meta name="referrer" content="origin">
     <script src="https://dcsaascdn.net/js/dc-sdk-1.0.5.min.js"></script>
-    <script>
-        (function () {
-            'use strict';
-
-            var styles;
-
-            if (localStorage.getItem('styles')) {
-                styles = JSON.parse(localStorage.getItem('styles'));
-                injectStyles(styles);
-            }
-
-            window.shopAppInstance = new ShopApp(function (app) {
-                app.init(null, function (params, app) {
-                    if (localStorage.getItem('styles') === null) {
-                        injectStyles(params.styles);
-                    }
-                    localStorage.setItem('styles', JSON.stringify(params.styles));
-
-                    app.show(null, function () {
-                        app.adjustIframeSize();
-                    });
-                }, function (errmsg, app) {
-                    alert(errmsg);
-                });
-            }, true);
-
-            function injectStyles(styles) {
-                var i;
-                var el;
-                var sLength;
-
-                sLength = styles.length;
-                for (i = 0; i < sLength; ++i) {
-                    el = document.createElement('link');
-                    el.rel = 'stylesheet';
-                    el.type = 'text/css';
-                    el.href = styles[i];
-                    document.getElementsByTagName('head')[0].appendChild(el);
-                }
-            }
-        }());
-    </script>
+    <script src="{{asset('js/shoper.js')}}"> </script>
 </head>
 
 <body>
 <div>
     <div>
         <form action="{{route('configure.save')}}" method="post">
-            @csrf
             <label for="website_id">
                 WebsiteId
                 <input type="text" id="website_id" name="website_id" max="128" value="{{$website_id ?? ''}}">
