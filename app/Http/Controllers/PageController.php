b243f5930363ca0ce7c39e8c44f6a45c6192e73b
Jakub <jakub.gloger@fingoweb.com>
2023-11-17 10:20:42 +0100

    remove website_id from files

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,14 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\DreamCommerceException;
 use App\Http\Requests\ConfigureShopIndexRequest;
-use App\Http\Requests\ConfigureShopRequest;
 use App\Models\Shop;
-use App\Models\ShopConfiguration;
-use App\Services\DreamCommerceService;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 
@@ -19,58 +13,7 @@ class PageController extends Controller
     {
         /** @var Shop $shop */
         $shop = Shop::where('shop', '=', $request->get('shop'))->first();
-        $shopAccessToken = $shop->access_token()->first()->access_token;
-        try {
-            $dreamCommerceService = new DreamCommerceService($shop->shop_url, $shopAccessToken, $shop);
-            $metaFields = $dreamCommerceService->getMetaFields();
-            $websiteId = $metaFields->get(DreamCommerceService::NAME_FOR_META_FIELD_WEBSITE_ID);
-        } catch (DreamCommerceException $e) {
-            return \response($e->getMessage(), 500);
-        }
 
-        return \view('configure', ['shop' => $shop, 'website_id' => $websiteId]);
-    }
-
-    public function save(ConfigureShopRequest $request): Response | RedirectResponse
-    {
-        $externalShopId = $request->get('shop_external_id');
-        $websiteId = $request->get('website_id');
-
-        /** @var Shop $shop */
-        $shop = Shop::where('shop', '=', $externalShopId)->firstOrFail();
-
-        /** @var ShopConfiguration $shopConfiguration */
-        ShopConfiguration::updateOrCreate(
-            ['shop_id' => $shop->id],
-            [
-                'website_id' => $websiteId,
-            ]
-        );
-
-        $shopAccessToken = $shop->access_token()->first()->access_token;
-        try {
-            $dreamCommerceService = new DreamCommerceService(
-                $shop->shop_url,
-                $shopAccessToken,
-                $shop
-            );
-            if (!$dreamCommerceService->checkMetaFieldsExists()) {
-                $dreamCommerceService->createMetaFields(
-                    $websiteId,
-                );
-            } else {
-                $metaFieldsIds = $dreamCommerceService->getMetaFields(true)->toArray();
-                $mappedMetaFieldsValues = $dreamCommerceService->mapNewMetaFieldsValuesToIds(
-                    $metaFieldsIds,
-                    $websiteId,
-                );
-
-                $dreamCommerceService->updateMetaFieldsValues($mappedMetaFieldsValues);
-            }
-        } catch (DreamCommerceException $exception) {
-            return \response($exception->getMessage(), 500);
-        }
-
-        return redirect(route('configure.index', ['shop' => $shop->shop]))->with(['success' => true]);
+        return \view('configure', ['shop' => $shop]);
     }
 }
--- resources/views/configure.blade.php
+++ resources/views/configure.blade.php
@@ -2,30 +2,7 @@
 @section('content')
     <div style="text-align: center;">
         <div>
-            <h3>Skonfiguruj website id dla sklepu: {{$shop->shop_url}}</h3>
-            <form action="{{route('configure.save')}}" method="post">
-                <label for="website_id">
-                    WebsiteId
-                    <input type="text" id="website_id" name="website_id" max="128" value="{{$website_id ?? ''}}">
-                </label>
-                <input type="hidden" value="{{$shop->shop}}" name="shop_external_id">
-                <br>
-                <input type="submit" value="Zapisz">
-                @if($errors->any())
-                    <div style="background-color: red;">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                @if(session('success'))
-                    <div style="background-color: green;">
-                        Wszystko okej, zapisano zmiany.
-                    </div>
-                @endif
-            </form>
+            <h3>Sklep: {{$shop->shop_url}}</h3>
         </div>
     </div>
 @endsection
